d52df5c6451b3c9d463f6bd8699c22dac7ae65cc
Dodododohoon <prot123480@gmail.com>
2025-09-21 19:19:08 +0900

    step5 ing

---
+++ BOJ/step5.py
@@ -0,0 +1,22 @@
+#27866
+#import sys
+
+#li = sys.stdin.readline().strip()
+#num = int(sys.stdin.readline())
+#print(li[num-1])
+
+#2743
+import sys
+li = sys.stdin.readline().strip()
+print(len(li))
+
+#9086
+
+#11654
+#11720
+#10809
+#2675
+#1152
+#2908
+#5622
+#11718
