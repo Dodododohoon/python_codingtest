6991b067f350ae50c3e6db36b180ea8b39141197
DOdododohoon <prot123480@gmail.com>
2025-09-24 00:21:31 +0900

    step5 ing

--- BOJ/step6.py
+++ BOJ/step6.py
@@ -24,7 +24,7 @@
 #         print(" " * (i - (n - 1)), "*" * (2 * ((2 * n - 2) - i) + 1), " ", sep="")
 
 # 10988
-import sys
+# import sys
 
 # li = sys.stdin.readline().strip()
 # if li == li[::-1]:
@@ -33,23 +33,83 @@ import sys
 #     print("0")
 
 # 1157
-import sys
+# import sys
 
-li = sys.stdin.readline().strip()
-li_ord = list(map(ord, li.upper()))  # 65'A'~90'Z'
-li_cnt = [0] * 26
+# li = sys.stdin.readline().strip()
+# li_ord = list(map(ord, li.upper()))  # 65'A'~90'Z'
+# li_cnt = [0] * 26
 
-for i in range(65, 91):
-    li_cnt[i - 65] = li_ord.count(i)
+# for i in range(65, 91):
+#     li_cnt[i - 65] = li_ord.count(i)
 
-if li_cnt.count(max(li_cnt)) == 1:
-    idx = li_cnt.index(max(li_cnt))
-    print(chr(idx + 65))
-else:
-    print("?")
+# if li_cnt.count(max(li_cnt)) == 1:
+#     idx = li_cnt.index(max(li_cnt))
+#     print(chr(idx + 65))
+# else:
+#     print("?")
 
 # 2941
+# import sys
+
+# li = sys.stdin.readline().strip()
+# li = li.replace("dz=", "*")
+# li = li.replace("c=", "*")
+# li = li.replace("c-", "*")
+# li = li.replace("d-", "*")
+# li = li.replace("lj", "*")
+# li = li.replace("nj", "*")
+# li = li.replace("s=", "*")
+# li = li.replace("z=", "*")
+# print(len(li))
 
 # 1316
+# import sys
+
+# num = int(sys.stdin.readline().strip())
+# cnt = 0
+
+# for _ in range(num):
+#     li = sys.stdin.readline().strip()
+#     check = 0
+#     for i in range(len(li)):
+#         if check == 1:
+#             break
+#         elif i == len(li) - 1:
+#             cnt += 1
+#             break
+#         elif li[i] == li[i + 1]:
+#             continue
+#         else:
+#             for j in range(i + 1, len(li)):
+#                 if li[j] == li[i]:
+#                     check = 1
+#                     break
+# print(cnt)
+# ===========================================
+# import sys
+
+# def is_group(s:str) -> bool:
+#     seen = set()
+#     prev = None
+#     for ch in s:
+#         if ch != prev:
+#             if ch in seen:
+#                 return False
+#             if prev is not None:
+#                 seen.add(prev)
+#         prev = ch
+#     return True
+
+# n=int(sys.stdin.readline())
+# ans = 0
+# for _ in range(n):
+#     if is_group(sys.stdin.readline().strip()):
+#         ans+=1
+# print(ans)
 
 # 25206
+import sys
+
+
+def lecture(s: str):
+    li = s.split()
