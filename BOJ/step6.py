1c21a5badf2a3c785baa62457cc1d136533d9323
DOdododohoon <prot123480@gmail.com>
2025-09-23 17:51:41 +0900

    step6 ing

--- BOJ/step6.py
+++ BOJ/step6.py
@@ -16,16 +16,37 @@
 #    print(li1[i] - li2[i], end=" ")
 
 # 2444
-n = int(input())
-for i in range(2 * n - 1):
-    if i <= n - 1:
-        print(" " * ((n - 1) - i), "*" * (2 * i + 1), " ", sep="")
-    else:
-        print(" " * (i - (n - 1)), "*" * (2 * ((2 * n - 2) - i) + 1), " ", sep="")
+# n = int(input())
+# for i in range(2 * n - 1):
+#     if i <= n - 1:
+#         print(" " * ((n - 1) - i), "*" * (2 * i + 1), " ", sep="")
+#     else:
+#         print(" " * (i - (n - 1)), "*" * (2 * ((2 * n - 2) - i) + 1), " ", sep="")
 
 # 10988
+import sys
+
+# li = sys.stdin.readline().strip()
+# if li == li[::-1]:
+#     print("1")
+# else:
+#     print("0")
 
 # 1157
+import sys
+
+li = sys.stdin.readline().strip()
+li_ord = list(map(ord, li.upper()))  # 65'A'~90'Z'
+li_cnt = [0] * 26
+
+for i in range(65, 91):
+    li_cnt[i - 65] = li_ord.count(i)
+
+if li_cnt.count(max(li_cnt)) == 1:
+    idx = li_cnt.index(max(li_cnt))
+    print(chr(idx + 65))
+else:
+    print("?")
 
 # 2941
 
--- Study/basis/day1.py
+++
@@ -1,8 +0,0 @@
-multiline = "Life is too short\nYou need python"
-print(multiline)
-
-print(len(multiline))
-print(multiline[3])
-
-a = multiline[0:4]
-print(a)
