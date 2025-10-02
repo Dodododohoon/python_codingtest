6df49a07ed0e10c42cb124becbd4179fe3f405e4
Dodododohoon <prot123480@gmail.com>
2025-10-02 16:20:21 +0900

    step10 ing

--- BOJ/step10.py
+++ BOJ/step10.py
@@ -5,35 +5,79 @@
 # print(A*B)
 
 # 1085
-import sys
+#import sys
 
-x, y, w, h = map(int, sys.stdin.readline().split())
+#x, y, w, h = map(int, sys.stdin.readline().split())
 
-if w - x > x:
-    num1 = x
-else:
-    num1 = w - x
+#if w - x > x:
+#    num1 = x
+#else:
+#    num1 = w - x
 
-if h - y > y:
-    num2 = y
-else:
-    num2 = h - y
+#if h - y > y:
+#    num2 = y
+#else:
+#    num2 = h - y
 
-if num1 > num2:
-    print(num2)
-else:
-    print(num1)
+#if num1 > num2:
+#    print(num2)
+#else:
+#    print(num1)
 
 # 3009
+#x1, y1 = map(int, input().split())
+#x2, y2 = map(int, input().split())
+#x3, y3 = map(int, input().split())
+#x4, y4 =0, 0
+
+#if x1==x2:
+#    x4=x3
+#if x1==x3:
+#    x4=x2
+#if x2==x3:
+#    x4=x1
+
+#if y1==y2:
+#    y4=y3
+#if y1==y3:
+#    y4=y2
+#if y2==y3:
+#    y4=y1
+
+#print(x4, y4)
 
 
 # 15894
-
+#n = int(input())
+#print(n*2 + n*2)
 
 # 9063
+#n= int(input())
+#li_x=[]
+#li_y=[]
 
+#for i in range(n):
+#    x, y = map(int,input().split())
+#    li_x.append(x)
+#    li_y.append(y)
+#wid = max(li_x) - min(li_x)
+#lenth = max(li_y) - min(li_y)
+
+#print(wid * lenth)
 
 # 10101
+#a=int(input())
+#b=int(input())
+#c=int(input())
+
+#if a+b+c != 180:
+#    print('Error')
+#elif a==b==c:
+#    print("Equilateral")
+#elif a==b or b==c or a==c:
+#    print("Isosceles")
+#else:
+#    print('Scalene')
 
 
 # 5073
