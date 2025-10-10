30fbb80964572fdbe7023dd079d91b4f938487ba
Dodododohoon <prot123480@gmail.com>
2025-10-10 15:15:31 +0900

    step15_NumberTheory start

---
+++ BOJ/step15_NumberTheory.py
@@ -0,0 +1,60 @@
+#1934
+#def gcd(a,b):
+#    while b:
+#        a, b = b, a%b
+#    return a
+
+#def lcm(a,b):
+#    return a * b //gcd(a,b)
+
+#t = int(input())
+
+#for _ in range(t):
+#    n, m = map(int,input().split())
+#    print(lcm(n,m))
+    
+#13241
+#def gcd(a,b):
+#    while b:
+#        a, b = b, a%b
+#    return a
+
+#def lcm(a,b):
+#    return a * b //gcd(a,b)
+
+#n, m = map(int,input().split())
+#print(lcm(n,m))
+
+#1735
+#def gcd(a,b):
+#    while b:
+#        a,b=b,a%b
+#    return a
+
+#def lcm(a,b):
+#    return a*b//gcd(a,b)
+
+#a1, a2 = map(int, input().split())
+#b1, b2 = map(int, input().split())
+#c1, c2 = a1*b2+b1*a2,a2*b2
+
+#k=gcd(c1,c2)
+#print(c1//k, c2//k)
+
+
+#2485
+
+
+#4134
+
+
+#1929
+
+
+#4948
+
+
+#17103
+
+
+#13909
