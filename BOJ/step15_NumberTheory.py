afe62d96fbd6417dd77ff1496d9e66ddb64c82f3
DOdododohoon <prot123480@gmail.com>
2025-10-11 17:12:32 +0900

    step 15 ing

--- BOJ/step15_NumberTheory.py
+++ BOJ/step15_NumberTheory.py
@@ -1,60 +1,90 @@
-#1934
-#def gcd(a,b):
+# 1934
+# def gcd(a,b):
 #    while b:
 #        a, b = b, a%b
 #    return a
 
-#def lcm(a,b):
+# def lcm(a,b):
 #    return a * b //gcd(a,b)
 
-#t = int(input())
+# t = int(input())
 
-#for _ in range(t):
+# for _ in range(t):
 #    n, m = map(int,input().split())
 #    print(lcm(n,m))
-    
-#13241
-#def gcd(a,b):
+
+# 13241
+# def gcd(a,b):
 #    while b:
 #        a, b = b, a%b
 #    return a
 
-#def lcm(a,b):
+# def lcm(a,b):
 #    return a * b //gcd(a,b)
 
-#n, m = map(int,input().split())
-#print(lcm(n,m))
+# n, m = map(int,input().split())
+# print(lcm(n,m))
 
-#1735
-#def gcd(a,b):
+# 1735
+# def gcd(a,b):
 #    while b:
 #        a,b=b,a%b
 #    return a
 
-#def lcm(a,b):
+# def lcm(a,b):
 #    return a*b//gcd(a,b)
 
-#a1, a2 = map(int, input().split())
-#b1, b2 = map(int, input().split())
-#c1, c2 = a1*b2+b1*a2,a2*b2
+# a1, a2 = map(int, input().split())
+# b1, b2 = map(int, input().split())
+# c1, c2 = a1*b2+b1*a2,a2*b2
 
-#k=gcd(c1,c2)
-#print(c1//k, c2//k)
+# k=gcd(c1,c2)
+# print(c1//k, c2//k)
 
-
-#2485
-
-
-#4134
+# 2485
+import sys
 
 
-#1929
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
 
 
-#4948
+input = sys.stdin.readline
+
+n = int(input())
+a = int(input())
+s1 = set()
+s1.add(a)
+gap = 1000000000
+
+for _ in range(n - 1):
+    b = int(input())
+    s1.add(b)
+    k = b - a
+    g = gcd(k, gap)
+    if gap > k:
+        gap = g
+
+total_tree = (b - a) // gap + 1
+add_tree = total_tree - len(s1)
+
+print(gap)
+print(total_tree)
+print(add_tree)
 
 
-#17103
+# 4134
 
 
-#13909
+# 1929
+
+
+# 4948
+
+
+# 17103
+
+
+# 13909
