391d9ebfbc285ae933971287c15c9702c4610c32
DOdododohoon <prot123480@gmail.com>
2025-10-17 16:40:18 +0900

    step19 ing

--- BOJ/step19_recursion.py
+++ BOJ/step19_recursion.py
@@ -1,11 +1,39 @@
 # 27433
-def fect(x):
-    if x == 1 or x == 0:
+# def fect(x):
+#     if x == 1 or x == 0:
+#         return 1
+#     else:
+#         return fect(x - 1) * x
+
+# n = int(input())
+
+# print(fect(n))
+
+
+# 10870
+def pibo(x):
+    if x == 0:
+        return 0
+    elif x == 1:
         return 1
     else:
-        return fect(x - 1) * x
+        return pibo(x - 1) + pibo(x - 2)
 
 
 n = int(input())
+print(pibo(n))
 
-print(fect(n))
+
+# 25501
+
+
+# 24060
+
+
+# 4779
+
+
+# 2447
+
+
+# 11729
