dd21e4ffc93eb45169859ba016cbbde6a969d6da
DOdododohoon <prot123480@gmail.com>
2025-10-19 18:40:25 +0900

    step 17 ing

---
+++ BOJ/step17_combination.py
@@ -0,0 +1,15 @@
+# #15439
+# n = int(input())
+
+# print(n*(n-1))
+
+# 24723
+# n= int(input())
+# print(2**n)
+
+# 10872
+# n=int(input())
+# num=1
+# for i in range(1,n+1):
+#   num *=i
+# print(num)
