06b2f7da390a31d01a0d77d99b0ef42a8b086fc6
Dodododohoon <prot123480@gmail.com>
2025-11-19 14:51:11 +0900

    step 29 ing

--- BOJ/step29_ShortestPath.py
+++ BOJ/step29_ShortestPath.py
@@ -44,10 +44,87 @@
 #    else:
 #        print(dist[i])
 
-
-
 #1504
+#import sys
+#import heapq
 
+#input = sys.stdin.readline
+
+#n,e = map(int, input().split())
+
+#INF = int(1e9)
+
+#graph = [[] for _ in range(n+1)]
+#dist_a = [INF] * (n+1)
+#dist_b = [INF] * (n+1)
+#dist_c = [INF] * (n+1)
+#dist_a[1] = 0
+
+#for _ in range (e):
+#    a,b,c = map(int, input().split())
+#    graph[a].append((b,c))
+#    graph[b].append((a,c))
+#    
+#v1,v2 = map(int,input().split())
+
+#dist_b[v1] = 0
+#dist_c[v2] = 0
+
+#hq = [(0,1)]
+
+#while hq:
+#    cur_dist, u = heapq.heappop(hq)
+#    
+#    if cur_dist > dist_a[u]:
+#        continue
+#    
+#    for nxt, w in graph[u]:
+#        nd = cur_dist + w
+#        if dist_a[nxt] > nd:
+#            dist_a[nxt] = nd
+#            heapq.heappush(hq, (nd,nxt))
+
+#hq = [(0,v1)]
+
+#while hq:
+#    cur_dist, u = heapq.heappop(hq)
+#    
+#    if cur_dist > dist_b[u]:
+#        continue
+#    
+#    for nxt, w in graph[u]:
+#        nd = cur_dist + w
+#        if dist_b[nxt] > nd:
+#            dist_b[nxt] = nd
+#            heapq.heappush(hq, (nd,nxt))
+
+
+#hq = [(0,v2)]
+
+#while hq:
+#    cur_dist, u = heapq.heappop(hq)
+#    
+#    if cur_dist > dist_c[u]:
+#        continue
+#    
+#    for nxt, w in graph[u]:
+#        nd = cur_dist + w
+#        if dist_c[nxt] > nd:
+#            dist_c[nxt] = nd
+#            heapq.heappush(hq, (nd,nxt))
+
+#ans1 = dist_a[v1] + dist_b[v2] +dist_c[n]
+#ans2 = dist_a[v2] + dist_c[v1] +dist_b[n]
+#k = min(ans1,ans2)
+
+#if k >= INF:
+#    print('-1')
+#else:
+#    print(k)
+
+#오~~!!! 꽤 빡센거 같은데 Self로 풀었음. 
+#약간 구조가 길어져서 설마 이거 일까 했는데 맞음.
+#다익스트라 세번 돌렸는데 그냥 함수로 만들어서 3번 돌리는게 깔끔.
 
 #13549
 
