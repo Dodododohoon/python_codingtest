f347d8e6240986619d183d589692886c63434a11
DOdododohoon <prot123480@gmail.com>
2025-12-09 22:21:23 +0900

    Review_Prefix

--- Study/Algorithm_Review.py
+++ Study/Algorithm_Review.py
@@ -1,16 +1,16 @@
 ##BackTracking
 
-#15649 - 바로 안 떠올랐음.
-#import sys
-#sys.setrecursionlimit(10**6)
+# 15649 - 바로 안 떠올랐음.
+# import sys
+# sys.setrecursionlimit(10**6)
 
-#input = sys.stdin.readline
+# input = sys.stdin.readline
 
-#n, m = map(int, input().split())
-#visited = [False] * (n+1)
-#arr=[]
+# n, m = map(int, input().split())
+# visited = [False] * (n+1)
+# arr=[]
 
-#def BT(num):
+# def BT(num):
 #    if num == m:
 #        print(' '.join(map(str, arr)))
 #        return
@@ -21,18 +21,18 @@
 #            BT(num+1)
 #            arr.pop()
 #            visited[i] = False
-#            
-#BT(0)
+#
+# BT(0)
 
-#15650
-#import sys
-#input = sys.stdin.readline
+# 15650
+# import sys
+# input = sys.stdin.readline
 
-#n, m = map(int, input().split())
+# n, m = map(int, input().split())
 
-#arr=[]
+# arr=[]
 
-#def bt(k):
+# def bt(k):
 #    if len(arr) == m:
 #        print(' '.join(map(str, arr)))
 #        return
@@ -40,49 +40,78 @@
 #        arr.append(i)
 #        bt(i+1)
 #        arr.pop()
-#                    
-#bt(1) 
-#---------------------------------------------------------
+#
+# bt(1)
+# ---------------------------------------------------------
 ##Dynamic Programming - 점화식이 메인
-#2579
-#import sys
+# 2579
+# import sys
 
-#input = sys.stdin.readline
-#n = int(input())
-#arr = [int(input()) for _ in range(n)]
-#arr.insert(0,0)
+# input = sys.stdin.readline
+# n = int(input())
+# arr = [int(input()) for _ in range(n)]
+# arr.insert(0,0)
 
-#if n ==1:
+# if n ==1:
 #    print(arr[1])
 #    exit()
-#if n==2:
+# if n==2:
 #    print(arr[1]+arr[2])
 #    exit()
 
-#dp = [0] * (n+1)
-#dp[1] = arr[1]
-#dp[2] = arr[1]+arr[2]
+# dp = [0] * (n+1)
+# dp[1] = arr[1]
+# dp[2] = arr[1]+arr[2]
 
 
-#for i in range(3, n+1):
+# for i in range(3, n+1):
 #    dp[i] = max(dp[i-2], dp[i-3]+arr[i-1]) + arr[i]
 
-#print(dp[n])
-#--------------------------------------------------
+# print(dp[n])
+# --------------------------------------------------
 ##Prefix Sum - 1차원은 일단 깔끔, 2차원도 적응해보자.
-#11659
-#import sys
-#input = sys.stdin.readline
+# 11659
+# import sys
+# input = sys.stdin.readline
 
-#n,m = map(int, input().split())
+# n,m = map(int, input().split())
 
-#li = list(map(int, input().split()))
+# li = list(map(int, input().split()))
 
-#prefix = [0] * (n+1)
+# prefix = [0] * (n+1)
 
-#for i in range(1,n+1):
+# for i in range(1,n+1):
 #    prefix[i] = prefix[i-1] + li[i-1]
 
-#for _ in range(m):
+# for _ in range(m):
 #    i, j =map(int, input().split())
 #    print(prefix[j] - prefix[i-1])
+
+# 11660
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+prefix = [[0] * (n + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        prefix[i][j] = (
+            prefix[i - 1][j]
+            + prefix[i][j - 1]
+            - prefix[i - 1][j - 1]
+            + arr[i - 1][j - 1]
+        )
+
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+    print(
+        prefix[x2][y2]
+        - prefix[x1 - 1][y2]
+        - prefix[x2][y1 - 1]
+        + prefix[x1 - 1][y1 - 1]
+    )
