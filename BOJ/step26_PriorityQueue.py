3163fae0ff963de3887bb7be36946e1cb2548a03
DOdododohoon <prot123480@gmail.com>
2026-01-24 02:35:19 +0900

    01.24

--- BOJ/step26_PriorityQueue.py
+++ BOJ/step26_PriorityQueue.py
@@ -1,19 +1,19 @@
-#우선순위가 가장 높은 데이터를 가장 먼저 삭제하는 자료구조
+# 우선순위가 가장 높은 데이터를 가장 먼저 삭제하는 자료구조
 # heapq 이용. 파이썬에서는 min heap이 기본 구조.
 
-#11279
-#import sys
-#import heapq
+# 11279
+# import sys
+# import heapq
 
-#input=sys.stdin.readline
+# input=sys.stdin.readline
 
-#n = int(input())
+# n = int(input())
 
-#heap = []    
+# heap = []
 
-#for _ in range(n):
+# for _ in range(n):
 #    x = int(input())
-#    
+#
 #    if x:
 #        heapq.heappush(heap,-x)
 #    else:
@@ -22,40 +22,40 @@
 #            print(-k)
 #        else:
 #            print('0')
-        
 
-#1927
-#import sys
-#import heapq
 
-#input=sys.stdin.readline
+# 1927
+# import sys
+# import heapq
 
-#n = int(input())
+# input=sys.stdin.readline
 
-#heap = []    
-#for _ in range(n):
+# n = int(input())
+
+# heap = []
+# for _ in range(n):
 #    x = int(input())
-#    
+#
 #    if x:
 #        heapq.heappush(heap,x)
 #    else:
 #        if heap:
 #            print(heapq.heappop(heap))
 #        else:
-#            print('0') 
+#            print('0')
 
-#11286
-#import sys
-#import heapq
+# 11286
+# import sys
+# import heapq
 
-#input = sys.stdin.readline
+# input = sys.stdin.readline
 
-#heap =[]
-#n = int(input())
+# heap =[]
+# n = int(input())
 
-#for _ in range(n):
+# for _ in range(n):
 #    x = int(input())
-#    
+#
 #    if x == 0:
 #        if heap:
 #            print(heapq.heappop(heap)[1])
@@ -63,77 +63,95 @@
 #            print('0')
 #    else:
 #        heapq.heappush(heap,(abs(x),x))
-        
 
-#2075
-#import heapq
-#import sys
 
-#input = sys.stdin.readline
+# 2075
+# import heapq
+# import sys
 
-#n = int(input())
-#h = list(map(int, input().split()))
-#heapq.heapify(h)
+# input = sys.stdin.readline
 
-#----------------------------------- 내 풀이.
-#for _ in range(n-1):
+# n = int(input())
+# h = list(map(int, input().split()))
+# heapq.heapify(h)
+
+# ----------------------------------- 내 풀이.
+# for _ in range(n-1):
 #    tmp = [-x for x in map(int, input().split())]
 #    heapq.heapify(tmp)
-#    
+#
 #    while -(tmp[0]) > h[0]:
 #        a = -heapq.heappop(tmp)
 #        heapq.heappop(h)
 #        heapq.heappush(h,a)
-#------------------------------------ 더 심플 버전
-#for _ in range(n-1):
+# ------------------------------------ 더 심플 버전
+# for _ in range(n-1):
 #    for x in map(int, input().split()):
 #        if x >h[0]:
 #            heapq.heapreplace(h,x)
 
 
-#print(h[0])
+# print(h[0])
 
-#2696                    꼭 다시 풀어보자.
-import sys
-import heapq
-input= sys.stdin.readline
+# # 2075
+# import sys
+# import heapq
 
-t = int(input())
-arr=[]
+# input = sys.stdin.readline
 
-for _ in range(t):
-    arr=[]
-    m = int(input())
-    for _ in range((m+9)//10):
-        arr.extend(map(int,input().split()))
-    
-    out = []
-    left = []
-    right = []
-    
-    for i in range(m):
-        if not left or arr[i] <= -left[0]:
-            heapq.heappush(left, -arr[i])
-        else:
-            heapq.heappush(right, arr[i]) 
-    
-        if len(left) > len(right) +1 :
-            heapq.heappush(right, -heapq.heappop(left))
-        elif len(left) < len(right):
-            heapq.heappush(left, -heapq.heappop(right))
-            
-        if i % 2 ==0:
-            out.append(-left[0])
-        
-    print((m+1)//2)
-    for i in range(len(out)):
-        if i != 0 and i % 10 ==0:
-            print('')
-        print(out[i], end=' ')
+# n = int(input())
+# hq = list(map(int, input().split()))
+# heapq.heapify(hq)
+
+# for _ in range(n - 1):
+#     arr = list(map(int, input().split()))
+#     for x in arr:
+#         if x > hq[0]:
+#             heapq.heappop(hq)
+#             heapq.heappush(hq, x)
+# print(hq[0])
 
 
+# 2696                    꼭 다시 풀어보자.
+# import sys
+# import heapq
+# input= sys.stdin.readline
 
-#for _ in range(t):                나의 쓰레기 같은 풀이. BUT 온몸비틀기는 했다 ^^
+# t = int(input())
+# arr=[]
+
+# for _ in range(t):
+#     arr=[]
+#     m = int(input())
+#     for _ in range((m+9)//10):
+#         arr.extend(map(int,input().split()))
+
+#     out = []
+#     left = []
+#     right = []
+
+#     for i in range(m):
+#         if not left or arr[i] <= -left[0]:
+#             heapq.heappush(left, -arr[i])
+#         else:
+#             heapq.heappush(right, arr[i])
+
+#         if len(left) > len(right) +1 :
+#             heapq.heappush(right, -heapq.heappop(left))
+#         elif len(left) < len(right):
+#             heapq.heappush(left, -heapq.heappop(right))
+
+#         if i % 2 ==0:
+#             out.append(-left[0])
+
+#     print((m+1)//2)
+#     for i in range(len(out)):
+#         if i != 0 and i % 10 ==0:
+#             print('')
+#         print(out[i], end=' ')
+
+
+# for _ in range(t):                나의 쓰레기 같은 풀이. BUT 온몸비틀기는 했다 ^^
 #    m = int(input())
 #    k = (m-1) // 10
 #    for i in range(k+1):
@@ -141,7 +159,7 @@ for _ in range(t):
 #        arr = arr + temp
 #    h1=[]
 #    h2=[]
-#    
+#
 #    for i in range(m):
 #        heapq.heappush(h1, arr[i])
 #        if i%2 ==0:
@@ -158,4 +176,4 @@ for _ in range(t):
 #                cnt -= 1
 #            h1 = list(h2)
 
-#1202
+# 1202
--- BOJ/step34_UnionFind.py
+++ BOJ/step34_UnionFind.py
@@ -1,22 +1,22 @@
-#서로소 집합(Disjoint Set)을 관리하는 자료구조
-#대표 기능: 두 원소가 같은 그룹인지 확인(find), 두 그룹 합치기(union)
+# 서로소 집합(Disjoint Set)을 관리하는 자료구조
+# 대표 기능: 두 원소가 같은 그룹인지 확인(find), 두 그룹 합치기(union)
 
-#1717
-#import sys
-#sys.setrecursionlimit(10**6)
-#input = sys.stdin.readline
+# 1717
+# import sys
+# sys.setrecursionlimit(10**6)
+# input = sys.stdin.readline
 
-#n,m = map(int, input().split())
+# n,m = map(int, input().split())
 
-#parent = [i for i in range(n+1)]
-#rank = [0] * (n+1)
+# parent = [i for i in range(n+1)]
+# rank = [0] * (n+1)
 
-#def find(a):
+# def find(a):
 #    if parent[a] != a:
 #        parent[a] = find(parent[a])
 #    return parent[a]
 
-#def union(a, b):
+# def union(a, b):
 #    a = find(a)
 #    b = find(b)
 #    if a != b:
@@ -26,37 +26,75 @@
 #            parent[a]=b
 #            if rank[a] == rank[b]:
 #                rank[b] +=1
-#    
-#def uf(a,b):
+#
+# def uf(a,b):
 #    if find(a) == find(b):
 #        return 'YES'
 #    else:
 #        return 'NO'
 
-#for _ in range(m):
+# for _ in range(m):
 #    l,m,n = map(int,input().split())
 #    if l ==0:
 #        union(m,n)
 #    else:
 #        print(uf(m,n))
-        
-#1976
-#import sys
-#sys.setrecursionlimit(10**6)
-#input = sys.stdin.readline
 
-#n = int(input())
-#m = int(input())
+# # 1717
+# import sys
 
-#parent = [i for i in range(n+1)]
-#rank = [0] * (n+1)
+# input = sys.stdin.readline
 
-#def find(a):
+# n, m = map(int, input().split())
+
+# parents = [i for i in range(n + 1)]
+
+
+# def find(a):
+#     if parents[a] != a:
+#         parents[a] = find(parents[a])
+#     return parents[a]
+
+
+# def union(a, b):
+#     a = find(a)
+#     b = find(b)
+
+#     if a == b:
+#         return
+
+#     parents[b] = a
+
+
+# for _ in range(m):
+#     cal, a, b = map(int, input().split())
+
+#     if cal == 0:
+#         union(a, b)
+#     if cal == 1:
+#         if find(a) == find(b):
+#             print("yes")
+#         else:
+#             print("no")
+
+
+# 1976
+# import sys
+# sys.setrecursionlimit(10**6)
+# input = sys.stdin.readline
+
+# n = int(input())
+# m = int(input())
+
+# parent = [i for i in range(n+1)]
+# rank = [0] * (n+1)
+
+# def find(a):
 #    if parent[a] != a :
 #        parent[a] = find(parent[a])
 #    return parent[a]
 
-#def union(a,b):
+# def union(a,b):
 #    a = find(a)
 #    b = find(b)
 #    if a != b:
@@ -67,55 +105,55 @@
 #            if rank[a] == rank[b]:
 #                rank[a] +=1
 
-#for i in range(n):
+# for i in range(n):
 #    li = list(map(int,input().split()))
 #    for j in range(n):
 #        if li[j]==1:
 #            union(i+1,j+1)
 
-#li = list(map(int,input().split()))
-#check = find(li[0])
+# li = list(map(int,input().split()))
+# check = find(li[0])
 
-#for x in li:
+# for x in li:
 #    if check != find(x):
 #        print('NO')
 #        break
-#else:
+# else:
 #    print("YES")
 
 
-#4195                            키랑 값이랑 연결 짓는 문제는 딕셔너리로. O(1)임.
-#import sys
-#sys.setrecursionlimit(10**6)
-#input = sys.stdin.readline
+# 4195                            키랑 값이랑 연결 짓는 문제는 딕셔너리로. O(1)임.
+# import sys
+# sys.setrecursionlimit(10**6)
+# input = sys.stdin.readline
 
-#t = int(input())
+# t = int(input())
 
-#for _ in range(t):
+# for _ in range(t):
 #    f = int(input())
 #    parent = [i for i in range(2*f +1)]
 #    dict = {}
 #    cnt = [1] * (2*f + 1)
 #    idx = 0
-#    
+#
 #    def find(a):
 #        if a != parent[a]:
 #            parent[a] = find(parent[a])
 #        return parent[a]
-#        
+#
 #    def union(a,b):
 #        a = find(a)
 #        b = find(b)
-#        
+#
 #        if a != b:
 #            parent[b] = a
 #            temp = cnt[a]
 #            cnt[a] += cnt[b]
 #            cnt[b] += temp
-#            
+#
 #    for _ in range(f):
 #        a,b = map(str, input().split())
-#        
+#
 #        if not a in dict:
 #            dict[a] = idx
 #            idx+=1
@@ -124,10 +162,8 @@
 #            dict[b] = idx
 #            idx +=1
 #        index_b = dict.get(b)
-#        
+#
 #        union(index_a,index_b)
 #        print(cnt[find(index_a)])
-        
-#20040
 
-
+# 20040
