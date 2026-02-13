63441fc7302828885c3434d024814f832964127c
Dodododohoon <prot123480@gmail.com>
2026-02-14 03:34:38 +0900

    02.13

--- BOJ/step34_UnionFind.py
+++ BOJ/step34_UnionFind.py
@@ -131,3 +131,48 @@
 #20040
 
 
+#---------------------
+#1976
+import sys
+input =sys.stdin.readline
+
+n = int(input())
+m = int(input())
+
+graph = [list(map(int, input().split())) for _ in range(n)]
+parent = [i for i in range(n+1)]
+rank = [0] * (n+1)
+
+check = list(map(int, input().split()))
+
+def find(v):
+    if parent[v] != v:
+        parent[v] = find(parent[v])
+    return parent[v]
+
+def union(a,b):
+    a = find(a)
+    b = find(b)
+    
+    if a==b:
+        return
+    
+    if rank[a] < rank[b]:
+        parent[a] = b
+    else:
+        parent[b] = a
+        if rank[a] == rank[b]:
+            rank[a] += 1
+       
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1:
+            union(i+1,j+1)
+
+root = parent[check[0]]
+for x in check[1:]:
+    if parent[x] != root:
+        print('NO')
+        exit()
+
+print('YES')
