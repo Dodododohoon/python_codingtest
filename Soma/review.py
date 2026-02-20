b2034b2bea77ecd88f68429c003713c1eb782fac
Dodododohoon <prot123480@gmail.com>
2026-02-21 00:02:46 +0900

    02.20

--- Soma/review.py
+++ Soma/review.py
@@ -185,4 +185,178 @@
 #    print(cnt-1)
         
         
-        
+#1806 - 부분합
+#import sys
+#input = sys.stdin.readline
+
+#n, s =map(int,input().split())
+#arr = list(map(int,input().split()))
+
+#ans = n + 1
+#total = 0
+
+#start, end = 0, 0
+#while start <= end:
+#    
+#    if end == n-1 and total < s:
+#        break
+#    if start == end:
+#        total = arr[start]
+#    
+#    if total >= s:
+#        ans = min(ans, end-start+1)
+#        if ans == 1:
+#            break
+#        total -= arr[start]
+#        start +=1
+#    else:
+#        if end < n-1:
+#            end += 1
+#            total += arr[end]
+#    
+#if ans == n+1:
+#    print('0')
+#else:
+#    print(ans)
+
+#1920
+#import sys
+#input = sys.stdin.readline
+
+#n = int(input())
+#arr = set(map(int,input().split()))
+#m = int(input())
+#check = list(map(int,input().split()))
+
+#for x in check:
+#    if x in arr:
+#        print('1')
+#    else:
+#        print('0')
+
+#arr.sort()
+
+#for x in check:
+#    left, right = 0, len(arr)-1
+#    
+#    while left <= right:
+#        half = (left + right)//2
+#        num = arr[half] 
+#        
+#        if num == x:
+#            print('1')
+#            break
+#        elif num > x:
+#            right = half - 1
+#        else:
+#            left = half + 1
+#    
+#    if num != x:
+#        print('0')
+
+#10816
+#import sys
+#from bisect import bisect_left, bisect_right
+#input = sys.stdin.readline
+
+#n = int(input())
+#arr = list(map(int, input().split()))
+#m = int(input())
+#check = list(map(int,input().split()))
+
+#arr.sort()
+
+#dic = dict()
+
+#for x in arr:
+#    if x in dic:
+#        dic[x] +=1
+#    else:
+#        dic[x] = 1
+
+#for x in check:
+#    if x in dic:
+#        print(dic[x])
+#    else:
+#        print('0')    
+
+#for x in check:
+#    left = bisect_left(arr,x)
+#    right = bisect_right(arr,x)
+#    if left < n:
+#        print(right - left, end=' ')
+#    else:
+#        print('0', end=' ')
+    
+#1167
+#import sys
+#from collections import deque
+#input = sys.stdin.readline
+
+#n = int(input())
+
+#graph = [[] for _ in range(n+1)]
+
+#for _ in range(n):
+#    arr = list(map(int, input().split()))
+#    
+#    for i in range(1,len(arr)-1,2):
+#        graph[arr[0]].append((arr[i],arr[i+1]))
+
+#def bfs(v):
+#    q = deque([(0,v)])
+#    
+#    while q:
+#        cur, w = q.popleft()
+#        
+#        for nxt, u in graph[w]:
+#            d = cur + u
+#            if dist[nxt] == -1:
+#                dist[nxt] = d
+#                q.append((d,nxt))
+
+#dist = [-1] * (n+1)
+#dist[1] = 0
+#bfs(1)
+
+#right = dist.index(max(dist))
+#dist = [-1] * (n+1)
+#dist[right] = 0
+#bfs(right)    
+#print(max(dist))
+
+#1976
+#import sys
+#input = sys.stdin.readline
+
+#n = int(input())
+#m = int(input())
+
+#parent = [i for i in range(n+1)]
+
+#def find(a):
+#    if a != parent[a]:
+#        parent[a] = find(parent[a])
+#    return parent[a]
+#    
+#def union(a,b):
+#    a=find(a)
+#    b=find(b)
+#    if a==b:
+#        return    
+#    parent[b] = a
+
+#for i in range(n):
+#    arr = list(map(int,input().split()))
+#    for j in range(len(arr)):
+#        if arr[j] == 1:
+#            union(i+1,j+1)
+
+#check = list(map(int,input().split()))
+#collect = find(check[0])
+#for x in check:
+#    if collect != find(x):
+#        print('NO')
+#        exit()
+#print('YES')
+
